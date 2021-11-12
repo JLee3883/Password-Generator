1f13111e4c1a836dfef373e3d5d6c049c98dce81
JLee <thishereisjenn@gmail.com>
2021-11-12 15:26:13 -0800

    added more thoughts on process to pseudo code

--- script.js
+++ script.js
@@ -11,7 +11,14 @@
 //     3.) Click OK to confirm including lowercase characters. (user has choice to OK or cancel)
 //     4.) Click OK to confirm including upperercase characters. (user has choice to OK or cancel)
 // Once user clicks OK, a random password containing all the chosen characters will automatically generate in the box.    
-//see and follow the path!!!
+//see and follow the path!!! creation of this password generator for the most idiotic person in the world. what happens if the user doesn't input the necessary data? how to alert if user doesnt follow the prompt properly. is that validation?
+// // put user input vairables:
+// put password variable values like:special characters,numeric characters,alphabetical characters,lower and upper
+//need choices declared outside the if statemnt so they can be concatenated upon condition.
+//convert letter to uppercase
+//create a variable for uppercase conversion
+//start functions to the generate button that asks for user input. validation??? then continuation once user input is validated.
+
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -26,6 +33,7 @@ function writePassword() {
 
 }
 function generatePassword() {
+window.alert("How many characters would you like your password to contain?")
 
 }
 // Add event listener to generate button
