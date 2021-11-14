bc8f810c6f43f3e0f1b6986b60ddb7754472dbd7
JLee <thishereisjenn@gmail.com>
2021-11-13 19:10:16 -0800

    updated and filled out all possible characters that would be available

--- script.js
+++ script.js
@@ -10,18 +10,29 @@
 //     2.) Click OK to confirm including numeric characters (user has choice to OK or cancel)
 //     3.) Click OK to confirm including lowercase characters. (user has choice to OK or cancel)
 //     4.) Click OK to confirm including upperercase characters. (user has choice to OK or cancel)
-// Once user clicks OK, a random password containing all the chosen characters will automatically generate in the box.    
+//!!!!!!!!!!!!!!!!!!!!!!! Once user clicks OK, a random password containing all the chosen characters will automatically generate in the box.!!!!!!!!!!!!!!!!!!!!!!!!!!    
 //see and follow the path!!! creation of this password generator for the most idiotic person in the world. what happens if the user doesn't input the necessary data? how to alert if user doesnt follow the prompt properly. is that validation?
 // // put user input vairables:
-// put password variable values like:special characters,numeric characters,alphabetical characters,lower and upper
+// put password variable values like:special characters,numeric characters,alphabetical characters,lower and upper JUST PUT ALL OF THE VARIABLES THAT YOU KNOW THAT YOU WILL NEED.
 //need choices declared outside the if statemnt so they can be concatenated upon condition.
 //convert letter to uppercase
 //create a variable for uppercase conversion
 //start functions to the generate button that asks for user input. validation??? then continuation once user input is validated.
+//vars, functions, ifs, else ifs, else 
+//if
 
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var confirmSpecial;
+var confirmUpper;
+var confirmLower;
+
+//All possible characters that would be availalbe.//
+var special = ["~","!","@","#","$","%","^","&","*","_","+","=","<",">","?",];
+var numbers = ["1","2","3","4","5","6","7","8","9","0",];
+var upper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+var lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 
 // Write password to the #password input
@@ -33,8 +44,20 @@ function writePassword() {
 
 }
 function generatePassword() {
-window.alert("How many characters would you like your password to contain?")
 
+var passLength = window.prompt("How many characters would you like your password to contain? Please choose a number between 8 and 128")
+// console.log(passLength);
+var confirmNum = window.confirm("Do you want to include numbers?")
+console.log(confirmNum)
+
+//confirm character
+//confirm upper
+//confirm lower
+//want to conpile the results - if user confirms upper and numbers, results would be combined array of upper and numbers. if true, creat me a password starting point that is combined upper array with the numbers array.
+//create combined array variable (whatever the user selects)
+//loop over combined array the number of times = to password length
+//each time we loop over, we are adding the random character to that "results"
+//return the results from this generate function to line 40 (var password=)
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
